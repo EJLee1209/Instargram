b5cc4d7d68b211cac2d012b597eacbf92d87fe0c
EJLee1209 <dldmswo1209@gmail.com>
2023-07-03 13:59:22 +0900

    Feat: Image selector
    use library : YPImagePicker

--- Instargram/Controller/MainTabController.swift
+++ Instargram/Controller/MainTabController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import Firebase
+import YPImagePicker
 
 class MainTabController: UITabBarController {
     private var user: User? {
@@ -47,6 +48,8 @@ class MainTabController: UITabBarController {
     
     //MARK: - Helpers
     func configureViewControllers(withUser user: User) {
+        self.delegate = self
+        
         // 탭바 뷰컨트롤러 설정
         let layout = UICollectionViewFlowLayout() // 컬렉션뷰를 초기화하기 위해 UICollectionViewFlowLayout() 인스턴스 생성 후 전달
         let feed = templateNavigationController(unselectedImage: #imageLiteral(resourceName: "home_unselected"), selectedImage: #imageLiteral(resourceName: "home_selected"), rootViewController: FeedController(collectionViewLayout: layout))
@@ -75,11 +78,46 @@ class MainTabController: UITabBarController {
         return nav
     }
     
+    func didFinishPickingMedia(_ picker: YPImagePicker) {
+        picker.didFinishPicking { items, _ in
+            picker.dismiss(animated: true) {
+                guard let selectedImage = items.singlePhoto?.image else { return }
+                print("DEBUG: Selected image is \(selectedImage)")
+            }
+        }
+    }
+    
 }
 //MARK: - AuthenticationDelegate
 extension MainTabController: AuthenticationDelegate {
     func authenticationComplete() {
         fetchUser() // 로그인시 유저 정보를 다시 가져옴
         self.dismiss(animated: true)
+    }
+}
+
+//MARK: - UITabBarControllerDelegate
+extension MainTabController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        let index = viewControllers?.firstIndex(of: viewController)
+        
+        if index == 2 { // 2번째 탭, 게시물 올리기 탭이 눌리면
+            var config = YPImagePickerConfiguration()
+            config.library.mediaType = .photo
+            config.shouldSaveNewPicturesToAlbum = false // 편집한 사진을 앨범에 저장할거야?
+            config.startOnScreen = .library
+            config.screens = [.library]
+            config.hidesStatusBar = false
+            config.hidesBottomBar = false
+            config.library.maxNumberOfItems = 1
+            
+            let picker = YPImagePicker(configuration: config)
+            picker.modalPresentationStyle = .fullScreen
+            present(picker, animated: true)
+            
+            didFinishPickingMedia(picker)
+        }
+        
+        return true
     }
 }
