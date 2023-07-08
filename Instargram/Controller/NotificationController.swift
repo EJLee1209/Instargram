39e36f0d890e3b4a8fbf5a4dbdc040e83667ece9
EJLee1209 <dldmswo1209@gmail.com>
2023-07-08 21:26:19 +0900

    UI: 알림 화면

--- Instargram/Controller/NotificationController.swift
+++ Instargram/Controller/NotificationController.swift
@@ -6,10 +6,52 @@
 //
 
 import UIKit
-class NotificationController: UIViewController {
+
+private let reuseIdentifier = "NotificationCell"
+
+class NotificationController: UITableViewController {
+    
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - LifeCycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureTableView()
         
-        view.backgroundColor = .systemOrange
+    }
+    
+    //MARK: - Helpers
+    func configureTableView() {
+        view.backgroundColor = .white
+        navigationItem.title = "Notifications"
+        
+        tableView.register(NotificationCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.rowHeight = 80
+        tableView.separatorStyle = .none
+        
     }
 }
+
+//MARK: - UITableViewDataSource
+
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! NotificationCell
+        return cell
+    }
+    
+}
+
+//MARK: - UITableViewDelegate
+extension NotificationController {
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
