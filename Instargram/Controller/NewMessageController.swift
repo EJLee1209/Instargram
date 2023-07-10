21b38ef4b401e16863bc67a98996d8da566a4509
EJLee1209 <dldmswo1209@gmail.com>
2023-07-10 22:43:27 +0900

    Feat: 유저 검색 기능 구현

--- Instargram/Controller/NewMessageController.swift
+++ Instargram/Controller/NewMessageController.swift
@@ -14,6 +14,9 @@ class NewMessageController: UITableViewController {
     //MARK: - Properties
     
     var users : [User] = []
+    var filteredUsers: [User] = [] // 검색어에 따라 유저 목록 필터링
+    
+    var searchMode = false
     
     //MARK: - LifeCycle
     
@@ -30,6 +33,7 @@ class NewMessageController: UITableViewController {
         view.backgroundColor = .white
         tableView.register(UserCell.self, forCellReuseIdentifier: reuseIdentifier)
         tableView.rowHeight = 64
+        
     }
     
     //MARK: - API
@@ -49,11 +53,11 @@ class NewMessageController: UITableViewController {
 
 extension NewMessageController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return users.count
+        return searchMode ? filteredUsers.count : users.count
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
-        cell.viewModel = UserCellViewModel(user: users[indexPath.row])
+        cell.viewModel = UserCellViewModel(user: searchMode ? filteredUsers[indexPath.row] : users[indexPath.row])
         return cell
     }
     // header 높이
@@ -81,6 +85,13 @@ extension NewMessageController {
 
 extension NewMessageController: NewMessageHeaderDelegate {
     func header(_ header: NewMessageHeader, textDidChanged searchText: String) {
-        print("DEBUG: \(searchText)")
+        searchMode = !searchText.isEmpty
+        
+        // 검색어로 유저 목록 필터링
+        filteredUsers = users.filter {
+            $0.username.contains(searchText) ||
+            $0.fullname.contains(searchText)
+        }
+        tableView.reloadData()
     }
 }
